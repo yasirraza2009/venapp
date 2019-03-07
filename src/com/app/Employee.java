b9ecd747cdc902646d5929a8411def5498b0fdca
yraza <yraza@Yraza>
2019-03-08 00:05:16 +0530

    1st file change

--- src/com/app/Employee.java
+++ src/com/app/Employee.java
@@ -2,24 +2,6 @@ package com.app;
 
 public class Employee {
 	private int empId;
-	private String empName;
-	private double empSal;
-
-	public String getEmpName() {
-		return empName;
-	}
-
-	public void setEmpName(String empName) {
-		this.empName = empName;
-	}
-
-	public double getEmpSal() {
-		return empSal;
-	}
-
-	public void setEmpSal(double empSal) {
-		this.empSal = empSal;
-	}
 
 	public int getEmpId() {
 		return empId;
