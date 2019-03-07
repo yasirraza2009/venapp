d649579a91740dff4947b1da5d54b1c9e3594395
yraza <yraza@Yraza>
2019-03-07 22:58:43 +0530

    1st file change

---
+++ src/com/app/Employee.java
@@ -0,0 +1,15 @@
+package com.app;
+
+public class Employee {
+	private int empId;
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	
+
+}
